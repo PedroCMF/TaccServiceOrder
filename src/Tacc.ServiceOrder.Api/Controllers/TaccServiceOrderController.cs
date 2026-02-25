0f996fd0e6bae0ead74569dae50674cc2e801411
pedroCMF <pedrounib@gmail.com>
2026-02-24 22:10:14 -0300

    feat: https://localhost:7108/api/TaccServiceOrder

---
+++ src/Tacc.ServiceOrder.Api/Controllers/TaccServiceOrderController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tacc.ServiceOrder.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TaccServiceOrderController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {       
+        return Ok(new {message = "Tacc ServiceOrder está rodando",date = DateTime.UtcNow} );
+    }
+}
